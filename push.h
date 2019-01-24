150be37a358c5898abf7f780401815260c6e2a42
RuanYunKeji <2391288572@qq.com>
2019-01-24 15:53:53 +0800

    提交工具类

---
+++ push.h
@@ -0,0 +1,17 @@
+//
+//  push.h
+//  ZJHVideoProcessing
+//
+//  Created by 安徽软云 on 2019/1/22.
+//  Copyright © 2019 ZhangJingHao2345. All rights reserved.
+//
+
+#ifndef push_h
+#define push_h
+
+#include <stdio.h>
+int pushMp4(const char *path);
+
+void close(void);
+
+#endif /* push_h */
